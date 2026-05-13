269ece5c08bc2860f2b55c4fa69407ecef11e359
prathyusha <chollangiprathyusha@gmail.com>
2026-05-13 10:25:08 +0530

    catalogue file

--- common.sh
+++ common.sh
@@ -9,6 +9,8 @@ Y="\e[33m"
 N="\e[0m"
 START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devopsd88s.online
 
 echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
 
@@ -25,6 +27,59 @@ VALIDATE(){
     else
         echo -e "$(date "+%Y-%m-%d %H-%M-%S") $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
+}
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NodeJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NodeJs Default 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Install NodeJS"
+
+    npm install &>>$LOGS_FILE ## you can write, if node js is install are not if not install , if yes install..
+    VALIDATE $? "Install Dependenices"
+
+}
+app_setup(){
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "Creating system user"
+    else 
+        echo -e "Roboshop user already exist..$Y Skipping $N"
+    fi
+
+    mkdir -p /app 
+    VALIDATE $? "Creating app directroy"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOGS_FILE
+    VALIDATE $? "Downloading $app_name code"
+
+    cd /app
+    VALIDATE $? "Moving to app directory" 
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "unzip $app_name code"
+}
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $app_name &>>$LOGS_FILE
+    systemctl start $app_name
+    VALIDATE $? "starting and enabling $app_name"
+}
+
+app_restart_setup(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarting $app_name"
+
 }
 
 print_total_time(){
